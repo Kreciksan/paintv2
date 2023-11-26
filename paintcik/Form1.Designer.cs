e50c2b5ea3459cde788d649757f914a10c2c0b6e
kacpe <kacpe@DESKTOP-JL6ACEU>
2023-11-26 22:16:35 +0100

    Add project files.

---
+++ paintcik/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace paintcik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.circleBtn = new System.Windows.Forms.Button();
+            this.rectangleBtn = new System.Windows.Forms.Button();
+            this.lineBtn = new System.Windows.Forms.Button();
+            this.eraserBtn = new System.Windows.Forms.Button();
+            this.fillBtn = new System.Windows.Forms.Button();
+            this.pencilBtn = new System.Windows.Forms.Button();
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBtn = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.stringTB = new System.Windows.Forms.TextBox();
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBtn);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.saveBtn);
+            this.panel1.Controls.Add(this.clearBtn);
+            this.panel1.Controls.Add(this.circleBtn);
+            this.panel1.Controls.Add(this.rectangleBtn);
+            this.panel1.Controls.Add(this.lineBtn);
+            this.panel1.Controls.Add(this.eraserBtn);
+            this.panel1.Controls.Add(this.fillBtn);
+            this.panel1.Controls.Add(this.pencilBtn);
+            this.panel1.Controls.Add(this.colorBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 88);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(36, 18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(878, 43);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(94, 25);
+            this.saveBtn.TabIndex = 8;
+            this.saveBtn.Text = "Zapisz";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(878, 3);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(94, 28);
+            this.clearBtn.TabIndex = 7;
+            this.clearBtn.Text = "Wyczyść";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // circleBtn
+            // 
+            this.circleBtn.Location = new System.Drawing.Point(790, 3);
+            this.circleBtn.Name = "circleBtn";
+            this.circleBtn.Size = new System.Drawing.Size(79, 65);
+            this.circleBtn.TabIndex = 6;
+            this.circleBtn.Text = "Koło";
+            this.circleBtn.UseVisualStyleBackColor = true;
+            this.circleBtn.Click += new System.EventHandler(this.circleBtn_Click);
+            // 
+            // rectangleBtn
+            // 
+            this.rectangleBtn.Location = new System.Drawing.Point(705, 3);
+            this.rectangleBtn.Name = "rectangleBtn";
+            this.rectangleBtn.Size = new System.Drawing.Size(79, 65);
+            this.rectangleBtn.TabIndex = 5;
+            this.rectangleBtn.Text = "Kwadrat";
+            this.rectangleBtn.UseVisualStyleBackColor = true;
+            this.rectangleBtn.Click += new System.EventHandler(this.rectangleBtn_Click);
+            // 
+            // lineBtn
+            // 
+            this.lineBtn.Location = new System.Drawing.Point(620, 3);
+            this.lineBtn.Name = "lineBtn";
+            this.lineBtn.Size = new System.Drawing.Size(79, 65);
+            this.lineBtn.TabIndex = 4;
+            this.lineBtn.Text = "Linia";
+            this.lineBtn.UseVisualStyleBackColor = true;
+            this.lineBtn.Click += new System.EventHandler(this.lineBtn_Click);
+            // 
+            // eraserBtn
+            // 
+            this.eraserBtn.Location = new System.Drawing.Point(535, 3);
+            this.eraserBtn.Name = "eraserBtn";
+            this.eraserBtn.Size = new System.Drawing.Size(79, 65);
+            this.eraserBtn.TabIndex = 3;
+            this.eraserBtn.Text = "Gumka ";
+            this.eraserBtn.UseVisualStyleBackColor = true;
+            this.eraserBtn.Click += new System.EventHandler(this.eraserBtn_Click);
+            // 
+            // fillBtn
+            // 
+            this.fillBtn.Location = new System.Drawing.Point(450, 3);
+            this.fillBtn.Name = "fillBtn";
+            this.fillBtn.Size = new System.Drawing.Size(79, 65);
+            this.fillBtn.TabIndex = 2;
+            this.fillBtn.Text = "Wypełnienie";
+            this.fillBtn.UseVisualStyleBackColor = true;
+            this.fillBtn.Click += new System.EventHandler(this.fillBtn_Click);
+            // 
+            // pencilBtn
+            // 
+            this.pencilBtn.Location = new System.Drawing.Point(365, 3);
+            this.pencilBtn.Name = "pencilBtn";
+            this.pencilBtn.Size = new System.Drawing.Size(79, 65);
+            this.pencilBtn.TabIndex = 1;
+            this.pencilBtn.Text = "Ołówek";
+            this.pencilBtn.UseVisualStyleBackColor = true;
+            this.pencilBtn.Click += new System.EventHandler(this.pencilBtn_Click);
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Location = new System.Drawing.Point(280, 3);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(79, 65);
+            this.colorBtn.TabIndex = 0;
+            this.colorBtn.Text = "Kolor";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 473);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBtn
+            // 
+            this.textBtn.Location = new System.Drawing.Point(195, 3);
+            this.textBtn.Name = "textBtn";
+            this.textBtn.Size = new System.Drawing.Size(79, 65);
+            this.textBtn.TabIndex = 10;
+            this.textBtn.Text = "Tekst";
+            this.textBtn.UseVisualStyleBackColor = true;
+            this.textBtn.Click += new System.EventHandler(this.textBtn_Click);
+            // 
+            // stringTB
+            // 
+            this.stringTB.Location = new System.Drawing.Point(391, 279);
+            this.stringTB.Name = "stringTB";
+            this.stringTB.Size = new System.Drawing.Size(225, 20);
+            this.stringTB.TabIndex = 2;
+            this.stringTB.Visible = false;
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.Location = new System.Drawing.Point(456, 305);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(75, 23);
+            this.confirmBtn.TabIndex = 3;
+            this.confirmBtn.Text = "Potwierdź";
+            this.confirmBtn.UseVisualStyleBackColor = true;
+            this.confirmBtn.Visible = false;
+            this.confirmBtn.Click += new System.EventHandler(this.confirmBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.confirmBtn);
+            this.Controls.Add(this.stringTB);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button circleBtn;
+        private System.Windows.Forms.Button rectangleBtn;
+        private System.Windows.Forms.Button lineBtn;
+        private System.Windows.Forms.Button eraserBtn;
+        private System.Windows.Forms.Button fillBtn;
+        private System.Windows.Forms.Button pencilBtn;
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button textBtn;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox stringTB;
+        private System.Windows.Forms.Button confirmBtn;
+    }
+}
+
---
+++ paintcik/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace paintcik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 1 pencil - 2 fill - 3 eraser - 4 line - 5 rectangle - 6 circle - 7 text
+        int action = 1;
+        Bitmap bm = new Bitmap(1000,500);
+        Bitmap picked = new Bitmap(100,50);
+        Graphics g;
+        Graphics pickedG;
+        SolidBrush brush = new SolidBrush(Color.Black);
+        Font font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
+        Rectangle PickedSize = new Rectangle(0, 0, 100, 50);
+        
+        Pen pen;
+        Pen eraser = new Pen(Color.White, 5);
+        Point p1, p2, sp;
+        bool paint = false;
+        Color color = Color.Black;
+
+        string text;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            g = Graphics.FromImage(bm);
+            pickedG = Graphics.FromImage(picked);
+            pen = new Pen(color, 2);
+            pictureBox1.Image = bm;
+            pictureBox2.Image = picked;
+            pickedG.FillRectangle(brush, PickedSize);
+            pictureBox2.Refresh();
+            saveFileDialog1.Filter = "Image(*.jpg)|*.jpg|(*.*)|*.*";
+        }
+
+        private void pencilBtn_Click(object sender, EventArgs e)
+        {
+            action = 1;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            pen.Color = color;
+            p1 = e.Location;
+            sp = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            p2 = e.Location;
+            Rectangle r = new Rectangle(sp.X, sp.Y, p2.X - sp.X, p2.Y - sp.Y);
+
+            if (action == 4)
+            {
+                g.DrawLine(pen, sp, p2);
+            }
+
+            if (action == 5)
+            {
+                g.DrawRectangle(pen, r);
+            }
+
+            if (action == 6)
+            {
+                g.DrawEllipse(pen, r);
+            }
+
+            paint = false;
+        }
+
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color = colorDialog1.Color;
+                brush.Color = color;
+                pickedG.FillRectangle(brush, PickedSize);
+                pictureBox2.Refresh();
+            }
+        }
+
+        private void fillBtn_Click(object sender, EventArgs e)
+        {
+            action = 2;
+        }
+
+        private void eraserBtn_Click(object sender, EventArgs e)
+        {
+            action = 3;
+        }
+
+        private void lineBtn_Click(object sender, EventArgs e)
+        {
+            action = 4;
+        }
+
+        private void rectangleBtn_Click(object sender, EventArgs e)
+        {
+            action = 5;
+        }
+
+        private void circleBtn_Click(object sender, EventArgs e)
+        {
+            action = 6;
+        }
+
+        private void textBtn_Click(object sender, EventArgs e)
+        {
+            action = 7;
+        }
+
+        private void Validate(Bitmap bm, Stack<Point>pixel,int x, int y,Color old_color,Color new_color)
+        {
+            Color cx = bm.GetPixel(x, y);
+            if(cx == old_color)
+            {
+                pixel.Push(new Point(x, y));
+                bm.SetPixel(x, y, new_color);
+            }
+        }
+
+        public void Fill(Bitmap bm, int x, int y, Color new_clr)
+        {
+            Color old_color = bm.GetPixel(x, y);
+            Stack<Point> pixel = new Stack<Point>();
+            pixel.Push(new Point(x, y));
+            bm.SetPixel(x, y, new_clr);
+
+            if (old_color == new_clr) return;
+
+            while(pixel.Count > 0)
+            {
+                Point pt = pixel.Pop();
+                if(pt.X > 0 && pt.Y > 0 && pt.X < bm.Width - 1 && pt.Y < bm.Height - 1)
+                {
+                    Validate(bm, pixel,pt.X - 1,pt.Y,old_color,new_clr);
+                    Validate(bm, pixel, pt.X, pt.Y - 1, old_color, new_clr);
+                    Validate(bm, pixel, pt.X +1, pt.Y, old_color, new_clr);
+                    Validate(bm, pixel, pt.X, pt.Y +1, old_color, new_clr);
+
+                }
+            }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (action == 2)
+            {
+                Fill(bm,e.X,e.Y,color);
+            }
+
+            if (action == 7)
+            {
+                if (fontDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    font = fontDialog1.Font;
+
+                }
+                sp = e.Location;
+                Point btnLocation = new Point(e.X, e.Y + 125);
+                Point tbLocation = new Point(e.X, e.Y + 100);
+                stringTB.Location = tbLocation;
+                confirmBtn.Location = btnLocation;
+                stringTB.Show();
+                confirmBtn.Show();
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            brush.Color = Color.White;
+            g.FillRectangle(brush, 0,0,1000,500);
+            pictureBox1.Refresh();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        { 
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bm.Save(saveFileDialog1.FileName,ImageFormat.Jpeg);
+            }
+        }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            text = stringTB.Text;
+            g.DrawString(text, font, brush, sp.X, sp.Y);
+            stringTB.Text = "";
+            stringTB.Visible = false;
+            confirmBtn.Visible = false;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+
+            if (paint)
+            {
+                if (action == 4)
+                {
+                    g.DrawLine(pen, sp, p2);
+                }
+
+                if (action == 5)
+                {
+                    g.DrawRectangle(pen, sp.X, sp.Y, p2.X - sp.X, p2.Y - sp.Y);
+                }
+
+                if (action == 6)
+                {
+                    g.DrawEllipse(pen, sp.X, sp.Y, p2.X - sp.X, p2.Y - sp.Y);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(paint)
+            {
+                p2 = e.Location;
+                if (action == 1)
+                {
+                    g.DrawLine(pen, p1, p2);
+                }
+ 
+                if (action == 3)
+                {
+                    g.DrawLine(eraser, p1, p2);
+                }
+                p1 = p2;
+
+            }
+            pictureBox1.Refresh();
+
+            
+        }
+    }
+}
